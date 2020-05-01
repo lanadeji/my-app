6da62c92eb55b8185e1fade7219b561d1ead9203
lanadeji <dejiolulana@gmail.com>
2020-05-01 13:09:53 -0600

    Updates

---
+++ precache-manifest.a5bf2ba095b71dfdddaf67638f7f4b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d985648d2a09697b02c9a52fa0c83514",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c5211e34191ecffb500e",
+    "url": "/my-app/static/css/main.d659efac.chunk.css"
+  },
+  {
+    "revision": "db7511ef2fcc00df4587",
+    "url": "/my-app/static/js/2.4fd1cbe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.4fd1cbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5211e34191ecffb500e",
+    "url": "/my-app/static/js/main.5407b9fc.chunk.js"
+  },
+  {
+    "revision": "f14810cf060fed0bdb21",
+    "url": "/my-app/static/js/runtime-main.9ce3c8af.js"
+  }
+]);
